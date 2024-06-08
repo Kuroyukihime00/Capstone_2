de58730b648dbd4002f48c991f523f5ad1416051
2172013 Raafi Khasha Brahmadito Haritsyah <37451728+kingsman15@users.noreply.github.com>
2024-06-08 10:44:38 +0700

    Add files via upload

--- controllers/facultyController.js
+++ controllers/facultyController.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // facultyController.js
 const Scholarship = require('../models/Scholarship');
 const Period = require('../models/Period');
@@ -49,3 +50,56 @@ exports.togglePeriodStatus = (req, res) => {
     res.status(200).send(results);
   });
 };
+=======
+// facultyController.js
+const Scholarship = require('../models/Scholarship');
+const Period = require('../models/Period');
+
+// Menampilkan daftar mahasiswa yang mengajukan beasiswa per periode
+exports.getScholarshipsByPeriod = (req, res) => {
+  const periodId = req.params.periodId;
+  Scholarship.findByPeriodId(periodId, (err, results) => {
+    if (err) return res.status(500).send(err);
+    res.status(200).send(results);
+  });
+};
+
+// Menyetujui atau menolak pengajuan beasiswa
+exports.approveScholarship = (req, res) => {
+  const scholarshipId = req.params.scholarshipId;
+  const status = req.body.status; // 'approved' atau 'rejected'
+
+  Scholarship.updateStatus(scholarshipId, status, (err, results) => {
+    if (err) return res.status(500).send(err);
+    res.status(200).send(results);
+  });
+};
+
+// Menampilkan daftar periode pengajuan beasiswa
+exports.getPeriods = (req, res) => {
+  Period.getAll((err, results) => {
+    if (err) return res.status(500).send(err);
+    res.status(200).send(results);
+  });
+};
+
+// Menambahkan periode pengajuan beasiswa baru
+exports.createPeriod = (req, res) => {
+  const data = req.body;
+  Period.create(data, (err, results) => {
+    if (err) return res.status(500).send(err);
+    res.status(201).send(results);
+  });
+};
+
+// Mengubah status aktif/nonaktif periode pengajuan beasiswa
+exports.togglePeriodStatus = (req, res) => {
+  const periodId = req.params.periodId;
+  const active = req.body.active; // true atau false
+
+  Period.updateStatus(periodId, active, (err, results) => {
+    if (err) return res.status(500).send(err);
+    res.status(200).send(results);
+  });
+};
+>>>>>>> e1bfd541160f7f67c3b778804d2e4d2bd71a9c36
